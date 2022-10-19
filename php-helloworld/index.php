ce5f9ec064ca282f0fb268baed88d0e28e09c817
Student User <student@workstation.lab.example.com>
2022-10-19 09:04:59 -0400

    hange

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,5 +1,4 @@
 <?php
 print "Hello, World! php version is " . PHP_VERSION . "\n";
-print "Hello, World! php version is " . PHP_VERSION . "\n";
-print "Hello, World! php version is " . PHP_VERSION . "\n";
+print "Line change";
 ?>
